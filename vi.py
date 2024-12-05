54863f3df5803fcbdbb802f86db613bc4c061528
brc0d3s <brc0d3s@gmail.com>
2024-12-05 20:46:06 +0300

    ML

--- vi.py
+++
@@ -1,9 +0,0 @@
-with open("hinton.txt", "r") as file:
-    content = file.read()
-
-sentences = content.split(".")  
-
-for i, sentence in enumerate(sentences, start=1):
-    sentence = sentence.strip()  
-    if sentence:  
-        print(f"{i}. {sentence}.")
